f16e1cbbf7354233e9139ce492ef555dfdc5c51e
webdev-ashishk <webdev.ashishk@gmail.com>
2023-04-09 17:38:15 +0530

    working fine

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-console.log("how!");
+console.log("welcome!");
 
-const url =
-  "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,sexist,explicit";
-
-// const url = "https://official-joke-api.appspot.com/jokes/random";
+const url = "https://official-joke-api.appspot.com/jokes/random";
 
 const p = document.getElementById("para");
 const btn = document.getElementById("btn");
+let result;
 async function jokeapi() {
   const promise = await fetch(url);
-  const result = await promise.json();
-  // console.log(result.joke);
-  p.textContent = `${result.joke}`;
+  result = await promise.json();
+  console.log(result);
+
+  p.textContent = `${result.setup}`;
+  punchline.innerHTML = "";
   // console.log(result);
 }
 
 btn.addEventListener("click", jokeapi);
-// setTimeout(() => {}, 1000);
 jokeapi();
+
+const btnPunch = document.getElementById("btnPunch");
+const punchline = document.getElementById("punchline");
+btnPunch.addEventListener("click", () => {
+  // const newPara = document.createElement("p");
+  // newPara.textContent = `${result.punchline}`;
+  // console.log(newPara);
+  punchline.innerHTML = `${result.punchline}`;
+});
